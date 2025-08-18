bff8bbfabff35c53767620b9f2c31082ae215a72
jesoria <jesoria@indra.es>
2025-08-18 16:23:00 +0200

    feat: dudas

--- entity-registry/src/container/EntityRegistry.ts
+++ entity-registry/src/container/EntityRegistry.ts
@@ -14,31 +14,35 @@ class EntityRegistry {
     constructor() {
         this.registry = [];
 
-        const classpathSearchResult: ClasspathElement[] = classpathService.getClassByDecorator('@App');
-        console.log("🚀 ~ SchemaMapRegistry ~ constructor ~ classpathSearchResult:", classpathSearchResult)
+        // const classpathSearchResult: ClasspathElement[] = classpathService.getClassByDecorator('@App');
+        // console.log("🚀 ~ SchemaMapRegistry ~ constructor ~ classpathSearchResult:", classpathSearchResult)
 
-        if (classpathSearchResult.length === 0 || classpathSearchResult.length > 1) {
-            throw new Error("There must be exactly one class with the @App decorator.");
-        }
+        // if (classpathSearchResult.length === 0 || classpathSearchResult.length > 1) {
+        //     throw new Error("There must be exactly one class with the @App decorator.");
+        // }
 
-        const mainClasspath = classpathSearchResult[0];
-        console.log("🚀 ~ SchemaMapRegistry ~ constructor ~ mainClasspath:", mainClasspath)
-        const mainComponent: Component | undefined = componentRegistry.getByName(mainClasspath.name);
-        console.log("🚀 ~ SchemaMapRegistry ~ constructor ~ mainComponent:", mainComponent)
+        // const mainClasspath = classpathSearchResult[0];
+        // console.log("🚀 ~ SchemaMapRegistry ~ constructor ~ mainClasspath:", mainClasspath)
+        // const mainComponent: Component | undefined = componentRegistry.getByName(mainClasspath.name);
+        // console.log("🚀 ~ SchemaMapRegistry ~ constructor ~ mainComponent:", mainComponent)
 
-        if (!mainComponent) {
-            throw new Error(`Main component not found for class ${mainClasspath.name}`);
-        }
+        // if (!mainComponent) {
+        //     throw new Error(`Main component not found for class ${mainClasspath.name}`);
+        // }
 
-        const metadataKeys = Reflect.getMetadataKeys(mainComponent.class);
-        console.log("🚀 ~ SchemaMapRegistry ~ constructor ~ metadataKeys:", metadataKeys)
-        const initRegistry = metadataKeys.filter(key => key.includes(`${METADATA_CONFIGURATION}=${METADATA_CONFIGURATION_DATA}`)).length > 0;
-        console.log("🚀 ~ SchemaMapRegistry ~ constructor ~ initRegistry:", initRegistry)
+        // const metadataKeys = Reflect.getMetadataKeys(mainComponent.class);
+        // console.log("🚀 ~ SchemaMapRegistry ~ constructor ~ metadataKeys:", metadataKeys)
+        // const initRegistry = metadataKeys.filter(key => key.includes(`${METADATA_CONFIGURATION}=${METADATA_CONFIGURATION_DATA}`)).length > 0;
+        // console.log("🚀 ~ SchemaMapRegistry ~ constructor ~ initRegistry:", initRegistry)
 
-        if (initRegistry) {
-            const entities = classpathService.getAll().filter(e => e.decorators.length === 0 || e.decorators.includes('@Entity'));
-            console.log("🚀 ~ SchemaMapRegistry ~ constructor ~ entities:", entities)
-        }
+        // if (initRegistry) {
+        //     const entities = classpathService.getAll().filter(e => e.decorators.length === 0 || e.decorators.includes('@Entity'));
+        //     console.log("🚀 ~ SchemaMapRegistry ~ constructor ~ entities:", entities)
+        // }
+    }
+
+    getEntityByName(name: string): Entity | undefined {
+        return this.registry.find(entity => entity.name === name);
     }
 }
 
